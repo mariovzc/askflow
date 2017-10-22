21cd1d7decc25378b20ec115ad577c98c7359ca4
¨Mario <mario.vizcainos@gmail.com>
2017-10-22 12:12:25 -0500

    created the question controllers methods

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,29 +1,53 @@
 class QuestionsController < ApplicationController
   skip_before_action :require_login, only: [:index, :show]
-  
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
   def index
+    @questions = Question.all
   end
 
   def show
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to questions_path, notice: 'La pregunta fue creada exitosamente!!!'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
+  def update
+    if @question.update(question_params)
+      redirect_to questions_path, notice: 'La pregunta ha sido Modificada con éxito'
+    else
+      render :edit
+    end
+  end
+
   def destroy
+    if @question.destroy
+      redirect_to questions_path, notice: "la pregunta fue eliminada con éxito"
+    else
+      redirect_to questions_path, notice: "la pregunta no fue eliminada"
+    end
   end
 
   private
 
   def set_question
+    @question = Question.find(params[:id])
   end
 
   def question_params
+    params.require(:question).permit(:name, :description)
   end
 end
