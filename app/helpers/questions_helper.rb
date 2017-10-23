8655ffee5e46bb715f7a116571fc72d9e6a810d2
¨Mario <mario.vizcainos@gmail.com>
2017-10-23 10:25:15 -0500

    added words translations to es-CO

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,4 +2,7 @@ module QuestionsHelper
   def form_title
     @question.new_record? ? "Nueva Pregunta" : "Editar Pregunta"
   end
+  def show_search_question
+    controller.action_name == "index" || controller.action_name == "show"
+  end
 end
