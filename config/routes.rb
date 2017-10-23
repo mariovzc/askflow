2bd85d993342f4a88fb43241be10d5aa7219d4ba
¨Mario <mario.vizcainos@gmail.com>
2017-10-22 21:23:17 -0500

    update of login route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get '/sign_up', to: 'users#new', as: :sign_up
   get '/log_in', to: 'sessions#new', as: :log_in
+  get '/sessions', to: 'sessions#new'
   delete '/log_out', to: 'sessions#destroy', as: :log_out
 
   resources :users, only: [:new, :create]  
