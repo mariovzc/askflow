2f8ceba729579b5b563e6f68d6caa4d2927cdc23
¨Mario <mario.vizcainos@gmail.com>
2017-11-08 12:05:44 -0500

    display comments validations

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,13 +3,17 @@ class CommentsController < ApplicationController
   before_action :set_comment_for_votes, only: [:upvote, :downvote, :unvote]
   def create
     @comment = @commentable.comments.new(comment_params)
-    @comment.save
+    unless @comment.save
+      flash[:danger] = "La respuesta  no puede estar vacio"
+    end
     redirect_to @commentable
   end
 
   def reply
     reply = @comment.replies.new(reply_params)
-    reply.save
+    unless reply.save
+      flash[:danger] = "El comentario no puede estar vacio"
+    end
     redirect_to question_path(@comment.commentable_id) 
   end
   
--- app/controllers/questions/comments_controller.rb
+++ app/controllers/questions/comments_controller.rb
@@ -4,6 +4,6 @@ class Questions::CommentsController < CommentsController
   private
 
     def set_commentable
-      @commentable = Question.find(params[:question_id])
+      @commentable = Question.friendly.find(params[:question_id])
     end
 end
